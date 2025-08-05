f8a66734357f4f0aa04b8fc0fc501c258f49aa6c
lavesh <kumarlavesh469@gmail.com>
2025-08-05 19:03:08 +0530

    Solved Problem Print len of a name  Maximilianus Alexander Montgomery Fitzgerald III

--- Practice_files/practice.py
+++ Practice_files/practice.py
@@ -3,5 +3,4 @@ print(f"Hello {name}.")
 x =input("what is your favoritre color? ")
 y = input("what is your favorite car?")
 z =input("what is your favorite food?")
-print=input(f"which one will you choose  among {x}, {y},{z} ?"
-        )
+print=input(f"which one will you choose  among {x},{y}, {z} ?") 
---
+++ Practice_files/print_len_string.py
@@ -0,0 +1,5 @@
+x = "Maximilianus Alexander Montgomery Fitzgerald III"
+print(len(x))
+
+ 
+ 
