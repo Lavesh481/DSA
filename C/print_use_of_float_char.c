c7eadc497a826761f4329513d738f29bdc3ffbd8
Lavesh481 <kumarlavesh469@gmail.com>
2025-08-26 18:12:00 +0530

    started c language

---
+++ C/print_use_of_float_char.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+    float my_number = 1.24;
+    char my_char = 'L';
+    printf("MY FAVORITE NUMBER IS : %f\n ", my_number);
+    printf("MY favorite CHARACTER IS :%c\n",my_char);
+    return 0 ;}
